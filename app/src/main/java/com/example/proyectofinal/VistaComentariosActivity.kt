9e26bdbfc4db8d3fbc3cd457e1456ecf4c2091ca
Luis Francisco <riostorres2@gmail.com>
2023-11-30 19:51:06 -0600

    Subiendo proyecto al repositorio, disponible las vistas

---
+++ app/src/main/java/com/example/proyectofinal/VistaComentariosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proyectofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VistaComentariosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vista_comentarios)
+    }
+}
