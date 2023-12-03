6f57f4e75121ec81df251691a8d37d388ac586f8
JALMarquez29 <marquez1alejandro@gmail.com>
2023-12-03 16:53:11 -0600

    Alex6

--- app/src/main/java/com/example/proyectofinal/CarritoActivity.kt
+++ app/src/main/java/com/example/proyectofinal/CarritoActivity.kt
@@ -5,28 +5,34 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class CarritoActivity : AppCompatActivity() {
+
+    private lateinit var lvCarrito: ListView
+    private lateinit var txtPago: TextView
+    private lateinit var txtPago2: TextView
+    private lateinit var txtPago3: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carrito)
 
-        val rvCarrito: RecyclerView = findViewById(R.id.rvCarrito)
-        rvCarrito.layoutManager = LinearLayoutManager(this)
-
-        // Configurar el espaciado entre elementos
-        val spacingInPixels = resources.getDimensionPixelSize(R.dimen.spacing)
-        rvCarrito.addItemDecoration(SpacesItemDecoration(spacingInPixels))
+        lvCarrito = findViewById(R.id.lvProductos)
+        txtPago = findViewById(R.id.txtPago)
+        txtPago2 = findViewById(R.id.txtPago2)
+        txtPago3 = findViewById(R.id.txtPago3)
 
         val adapter = CarritoAdapter(Carrito.getProductos())
-        rvCarrito.adapter = adapter
+        lvCarrito.adapter = adapter
 
         val imgRegreso: ImageView = findViewById(R.id.imgextra2)
         imgRegreso.setOnClickListener {
-            finish() // Cierra la actividad actual y regresa a la actividad anterior (PrincipalActivity)
+            finish()
         }
 
         val btnPagar: Button = findViewById(R.id.btnPagar)
@@ -37,19 +43,25 @@ class CarritoActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+        actualizarTextos()
     }
 
-    // Clase para configurar el espaciado entre elementos
-    class SpacesItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
-        override fun getItemOffsets(outRect: android.graphics.Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-            outRect.left = space
-            outRect.right = space
-            outRect.bottom = space
+    private fun actualizarTextos() {
 
-            // Añade este bloque si no quieres espaciado en el primer elemento
-            if (parent.getChildAdapterPosition(view) == 0) {
-                outRect.top = space
-            }
+        val subtotal = calcularSubtotal()
+        txtPago.text = "$ $subtotal"
+        val montoFijo = 100.00
+        txtPago2.text = "$ $montoFijo"
+        val total = subtotal + montoFijo
+        txtPago3.text = "$ $total"
+    }
+    private fun calcularSubtotal(): Double {
+        val productosEnCarrito = Carrito.getProductos()
+        var subtotal = 0.0
+
+        for (producto in productosEnCarrito) {
+            subtotal += producto.precio
         }
+        return subtotal
     }
 }
--- app/src/main/java/com/example/proyectofinal/CarritoAdapter.kt
+++ app/src/main/java/com/example/proyectofinal/CarritoAdapter.kt
@@ -3,26 +3,56 @@ package com.example.proyectofinal
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
+import android.widget.BaseAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CarritoAdapter(private val productos: List<Producto>) : RecyclerView.Adapter<CarritoAdapter.ViewHolder>() {
+class CarritoAdapter(private val productos: List<Producto>) : BaseAdapter() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_carrito, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val producto = productos[position]
-        holder.bind(producto)
-    }
-
-    override fun getItemCount(): Int {
+    override fun getCount(): Int {
         return productos.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    override fun getItem(position: Int): Any {
+        return productos[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+        val holder: ViewHolder
+
+        if (convertView == null) {
+            view = LayoutInflater.from(parent?.context).inflate(R.layout.item_carrito, parent, false)
+            holder = ViewHolder(view)
+            view.tag = holder
+        } else {
+            view = convertView
+            holder = view.tag as ViewHolder
+        }
+
+        val producto = getItem(position) as Producto
+        holder.bind(producto)
+
+        // Agregar un margen inferior solo para el último elemento
+        val params = ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        view.layoutParams = params
+
+        if (position == count - 1) {
+            params.bottomMargin = view.context.resources.getDimensionPixelSize(R.dimen.margin_bottom_last_item)
+        } else {
+            params.bottomMargin = 0
+        }
+
+        return view
+    }
+
+
+    class ViewHolder(itemView: View) {
         private val nombreProducto: TextView = itemView.findViewById(R.id.nombreProductoCarrito)
         private val precioProducto: TextView = itemView.findViewById(R.id.precioProductoCarrito)
 
