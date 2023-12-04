1e33ee8e95324a2304dd1d9c1902a781bb968dcd
JALMarquez29 <marquez1alejandro@gmail.com>
2023-12-03 18:15:58 -0600

    Alex8

--- app/src/main/java/com/example/proyectofinal/ProductoIndividualActivity.kt
+++ app/src/main/java/com/example/proyectofinal/ProductoIndividualActivity.kt
@@ -17,12 +17,25 @@ class ProductoIndividualActivity : AppCompatActivity() {
         val productos = obtenerListaDeProductos()
         val adapter = ProductoAdapter(productos)
         recyclerView.adapter = adapter
+
+        adapter.setOnItemClickListener(object : ProductoAdapter.OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                // Obtener el producto seleccionado
+                val productoSeleccionado = productos[position]
+
+                // Abrir DetalleActivity y pasar la información del producto
+                val intent = Intent(this@ProductoIndividualActivity, DetalleActivity::class.java)
+                intent.putExtra("producto", productoSeleccionado)
+                startActivity(intent)
+            }
+        })
     }
+
     private fun obtenerListaDeProductos(): List<Producto> {
         return listOf(
-            Producto(R.drawable.p1, "Impresora 1", 100.0, "Descripción 1"),
-            Producto(R.drawable.p2, "Producto 2", 150.0, "Descripción 2"),
-
+            Producto("Impresora 1", 100, R.drawable.impresora1),
+            Producto("Producto 2", 150, R.drawable.producto2),
+            // Agrega más productos según sea necesario
         )
     }
 }
