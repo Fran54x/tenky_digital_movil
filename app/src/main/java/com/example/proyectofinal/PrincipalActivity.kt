51b05af5e6bc31c4c588bcfd1e28630de6929474
JALMarquez29 <marquez1alejandro@gmail.com>
2023-12-03 18:28:34 -0600

    Alex10

--- app/src/main/java/com/example/proyectofinal/PrincipalActivity.kt
+++ app/src/main/java/com/example/proyectofinal/PrincipalActivity.kt
@@ -3,6 +3,7 @@ package com.example.proyectofinal
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,6 +25,12 @@ class PrincipalActivity : AppCompatActivity() {
         imgCarrito.setOnClickListener {
             // Abre la actividad del carrito
             val intent = Intent(this, CarritoActivity::class.java)
+            startActivity(intent)
+        }
+        val txtVer: TextView = findViewById(R.id.txtVer)
+        txtVer.setOnClickListener {
+            // Abrir Activity_ProductoIndividual al hacer clic en el TextView
+            val intent = Intent(this, ProductoIndividualActivity::class.java)
             startActivity(intent)
         }
 
