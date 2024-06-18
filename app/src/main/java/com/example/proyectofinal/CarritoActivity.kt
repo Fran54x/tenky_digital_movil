9e5c9f04b16128fbb38110c83b6f68a4628e0a0b
JALMarquez29 <marquez1alejandro@gmail.com>
2024-06-17 21:54:00 -0600

    Cambios para bd 5.0

--- app/src/main/java/com/example/proyectofinal/CarritoActivity.kt
+++ app/src/main/java/com/example/proyectofinal/CarritoActivity.kt
@@ -1,6 +1,13 @@
 package com.example.proyectofinal
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -8,6 +15,10 @@ import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -22,7 +33,9 @@ class CarritoActivity : AppCompatActivity() {
     private val db = Firebase.firestore
     private val productos = mutableListOf<Producto>()
     private lateinit var adapter: CarritoAdapter
-
+    private val CHANNEL_ID = "channel"
+    private val NOTIFICATION_ID = 123
+    private val PERMISSION_REQUEST_CODE = 101
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carrito)
@@ -48,6 +61,7 @@ class CarritoActivity : AppCompatActivity() {
             FirestoreUtils.limpiarCarritoFirestore(userId,
                 onSuccess = {
                     Carrito.clearCarrito()
+                    checkNotificationPermission()
                     val intent = Intent(this, PrincipalActivity::class.java)
                     startActivity(intent)
                     finish()
@@ -108,5 +122,42 @@ class CarritoActivity : AppCompatActivity() {
                 Toast.makeText(this, "Error al eliminar producto: ${e.message}", Toast.LENGTH_SHORT).show()
             }
         )
+    }
+    private fun checkNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), PERMISSION_REQUEST_CODE)
+            } else {
+                mostrarNotificacion("Pago Realizado")
+            }
+        } else {
+            mostrarNotificacion("Pago Realizado")
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private fun mostrarNotificacion(content: String) {
+        // Crear el canal de notificación (solo es necesario en Android Oreo y superior)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val name = "Canal de notificación"
+            val descriptionText = "Descripción del canal"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.baseline_notifications_active_24) // icono de la notificación
+            .setContentTitle(content)
+            .setContentText("Felicidades tu pago fue realizado con éxito")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true) // cancelar la notificación al hacer clic en ella
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(NOTIFICATION_ID, builder.build())
+        }
     }
 }
